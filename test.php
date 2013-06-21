dca8c487f80440ed23ff6fb82d2598d4e55cbe25
ptmhd <ptmhd23@gmail.com>
2013-06-21 22:21:07 +0300

    Update test.php
    just another test.php with different master public key

--- test.php
+++ test.php
@@ -4,18 +4,18 @@
 require 'addrgen.php';
 
 for ($i = 0; $i < 5; $i++) {
-	print addr_from_mpk('675b7041a347223984750fe3ab229df0c9f960e7ec98226b7182a2cb1990e39901feecf5a670f1d788ab29f626e20de424f049d216fc6f4c6ec42506763fa28e', $i) . "\n";
+	print addr_from_mpk('f7d391b80782b3f46ed5ba3c449485a90295c06c9678aa543f73f240175cffcdbd69aca634e9a36687e6fbcc107422bca58a6115fa66afad4ce2f2eea0a6e130', $i) . "\n";
 }
 
 /*
 
 should print:
 
-13EfJ1hQBGMBbEwvPa3MLeH6buBhiMSfCC
-1AZW6GGmsUizkHhrtg853Qnxk68vCx2gFq
-1M7ReiRrcYygYYLdyPS2cKQi7p9s9ViFS1
-1NcawzHFoECpDhz8hUCZp43hGjvdEB8DBA
-1PjcUN9kEBn3kvUmT2BXezwTG4RRvrqjRw
+1CksxZMsz4o4hSS7igWXbWYwL1aaXsW64x
+1DpowaMMPLmHgwMNNKC7NKxEXDdv4N4qv6
+17F1wHLEVMmwSaKnuEfgjaxGnwTbd848rS
+16NaFFLSBytDL6MKfqGtf7i3VomXS11kcA
+1FMq6dnSRGnLgDdoxDwGojkxaSMwHHy1Sj
 
 */
 
